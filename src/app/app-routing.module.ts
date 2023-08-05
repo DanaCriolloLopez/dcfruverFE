e6b0597c1345dca64b24eb157e112c79f6b216d8
Dana Criollo <dana.criollo.ljfj@gmail.com>
2023-08-05 14:48:59 -0500

    app-routing y app-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { EditarProductosComponent } from './editar-productos/editar-productos.component';
+import { ListaPedidosComponent } from './lista-pedidos/lista-pedidos.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  //Rutas que redirigen a mi página principal 
+  { path: 'principal', component: PaginaPrincipalComponent },
+  //Ruta que dirige al carrito de compras 
+  { path: 'carrito', component: CarritoComponent},
+  //Rutas que dirigen a las accciones con el producto 
+  { path: 'productos', component: ListaProductosComponent},
+  { path: 'productos/editar/:idproducto', component: EditarProductosComponent},
+  { path: 'productos/agregar', component: EditarProductosComponent},
+  //Ruta para listar pedidos
+  { path: 'pedidos', component: ListaPedidosComponent},
+  //Ruta Login
+  { path: 'inicio', component: LoginComponent},
+  //Cualquier otra ruta que pongan se va a redirigir a mi página principal
+  { path: '**', redirectTo:'/principal', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { ProductoService } from './shared/producto.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductModalComponent } from './product-modal/product-modal.component';
+import { UsuarioService } from './shared/usuario.service';
+import { ClienteService } from './shared/cliente.service';
+import { PedidoService } from './shared/pedido.service';
+import { PedidodetalleService } from './shared/pedidodetalle.service';
+import { CarritoComponent } from './carrito/carrito.component';
+import { CarritoComprasService } from './shared/carrito-compras.service';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { EditarProductosComponent } from './editar-productos/editar-productos.component';
+import { ListaPedidosComponent } from './lista-pedidos/lista-pedidos.component';
+import { CorreoService } from './shared/correo.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    ProductModalComponent,
+    CarritoComponent,
+    ListaProductosComponent,
+    EditarProductosComponent,
+    ListaPedidosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    ProductoService,
+    UsuarioService,
+    ClienteService,
+    PedidoService,
+    PedidodetalleService,
+    CarritoComprasService,
+    CorreoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
